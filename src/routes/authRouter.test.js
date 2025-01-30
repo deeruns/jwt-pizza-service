7205ca15eb1c86841b49893bc1516f7939cab8ec
Davin Thompson <davindthomp@gmail.com>
2025-01-30 12:38:59 -0700

    auth router set up with logout and register test

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,6 +1,67 @@
-//const request = require('supertest');
-//const app = require('./authRouter');
+const request = require('supertest');
+const app = require('../service');
+const { Role, DB } = require('../database/database.js');
 
-test('hello world', () => {
-    expect('hello' + ' world').toBe('hello world');
-  });
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+let registerRes;
+let loginRes;
+
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+  jest.setTimeout(60 * 1000 * 5); // 5 minutes
+}
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  testUserAuthToken = loginRes.body.token;
+  expect(loginRes.status).toBe(200);
+  expectValidJwt(loginRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  expect(loginRes.body.user).toMatchObject(expectedUser);
+});
+
+test('logout', async () => {
+  const logoutRes = (await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`));
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toMatch('logout successful');
+});
+
+test('register a new user', async () => {
+  const newName = randomName();
+  const newUser = { name: newName, email: newName + '@test.com', password: 'a' };
+  const registerRes2 = await request(app).post('/api/auth').send(newUser);
+  expect(registerRes2.status).toBe(200);
+  expectValidJwt(registerRes2.body.token);
+
+  const expectedUser = { ...newUser, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  //password is not returned in the response
+  expect(registerRes2.body.user).toMatchObject(expectedUser);
+});
+
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
+
+async function createAdminUser() {
+    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+    user.name = randomName();
+    user.email = user.name + '@admin.com';
+  
+    user = await DB.addUser(user);
+    return { ...user, password: 'toomanysecrets' };
+  }
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
